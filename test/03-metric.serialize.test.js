9cf5ddea88402f60f40f91ae41a6d939d3dc5e65
Pier Fumagalli <pier@usrz.com>
2018-08-23 12:49:22 +0200

    Test parsing of metric list

--- test/03-metric.serialize.test.js
+++ test/03-metric.serialize.test.js
@@ -1,28 +1,26 @@
 'use strict'
 
-const { parse, serialize } = require('../metric')
-const { Metric } = require('../metric/classes.js')
+const { Metric, parse, serialize } = require('../metric')
 const expect = require('chai').expect
 
 const uuid = 'd562b0f9-9068-4d01-880f-7df32b4bf9ad'
 const timestamp = Date.now()
 
-const metric = {
-  timestamp,
-  uuid,
-  name: 'metricName',
-  value: 'a value',
-}
-
 describe('Buffer Serialization', () => {
-  it('should serialize and re-parse a metric', () => {
-    let buffer = serialize(metric)
+  it('should serialize and re-parse a minimal metric', () => {
+    let buffer = serialize({
+      timestamp,
+      uuid,
+      name: 'metricName',
+      value: 'a value',
+    })
 
     expect(buffer).to.be.instanceof(Buffer)
 
     let result = parse(buffer)
 
     expect(result).to.be.instanceof(Metric)
+
     expect(result).to.eql({
       timestamp,
       uuid,
@@ -30,9 +28,83 @@ describe('Buffer Serialization', () => {
       value: 'a value',
       type: 'string',
       accountId: 0,
+      checkName: '', // it's always non null
+      streamTags: [], // always an empty array
+    })
+  })
+
+  it('should serialize and re-parse a complete metric', () => {
+    let buffer = serialize({
+      timestamp,
+      uuid,
+      name: 'metricName',
+      value: 999,
+      type: 'number',
+      checkName: 'theCheckName',
+      accountId: 65535,
+      streamTags: [ 'a:b', 'c:d' ],
     })
 
-    // console.log(result)
+    expect(buffer).to.be.instanceof(Buffer)
+
+    let result = parse(buffer)
+
+    expect(result).to.be.instanceof(Metric)
+
+    expect(result).to.eql({
+      timestamp,
+      uuid,
+      name: 'metricName',
+      value: 999,
+      type: 'number',
+      accountId: 65535,
+      checkName: 'theCheckName',
+      streamTags: [ 'a:b', 'c:d' ],
+    })
+  })
+
+  it('should serialize and re-parse an array of metrics', () => {
+    let buffer = serialize([ {
+      timestamp,
+      uuid,
+      name: 'metricName',
+      value: 'a value',
+    }, {
+      timestamp,
+      uuid,
+      name: 'metricName',
+      value: 999,
+      type: 'number',
+      checkName: 'theCheckName',
+      accountId: 65535,
+      streamTags: [ 'a:b', 'c:d' ],
+    } ])
+
+    expect(buffer).to.be.instanceof(Buffer)
+
+    let result = parse(buffer)
+
+    expect(result).to.be.an('array')
+
+    expect(result).to.eql([ {
+      timestamp,
+      uuid,
+      name: 'metricName',
+      value: 'a value',
+      type: 'string',
+      accountId: 0,
+      checkName: '', // it's always non null
+      streamTags: [], // always an empty array
+    }, {
+      timestamp,
+      uuid,
+      name: 'metricName',
+      value: 999,
+      type: 'number',
+      accountId: 65535,
+      checkName: 'theCheckName',
+      streamTags: [ 'a:b', 'c:d' ],
+    } ])
   })
 })
 
