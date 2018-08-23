c5638ab1da0065f29c6d76e41422a69d5e0d8a47
Pier Fumagalli <pier@usrz.com>
2018-08-23 12:46:19 +0200

    Export Metric class

--- metric/index.js
+++ metric/index.js
@@ -1,10 +1,14 @@
 'use strict'
 
+const Metric = require('./metric.js')
+
 const parse = require('./parse.js')
 const validate = require('./validate.js')
 const serialize = require('./serialize.js')
 
 module.exports = {
+  Metric,
+
   parse,
   validate,
   serialize,
